7e1568c1d141de063b538219b5273b8d806b97f4
Unknown <veronica.21.coutts@gmail.com>
2018-08-05 16:58:41 +0200

    Event added

--- PetShop.sol
+++ PetShop.sol
@@ -2,11 +2,13 @@ pragma solidity ^0.4.17;
 
 contract Adoption {
     address[17] public adopters;
+    event petAdoption(uint petID, address adopter);
 
     function adopt (uint petID) public returns(uint) {
         require(petID >= 0 && petID <= 16, "Invalid petID");
         require(adopters[petID] == 0x0000000000000000000000000000000000000000, "Pet already adopted");
         adopters[petID] = msg.sender;
+        emit petAdoption(petID, msg.sender);
         return petID;
     }
 
