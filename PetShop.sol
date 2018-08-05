83f41a178c891581d3a308b5f8b245f58b6e7ab3
Unknown <veronica.21.coutts@gmail.com>
2018-08-05 16:50:16 +0200

    Extra

--- PetShop.sol
+++ PetShop.sol
@@ -2,14 +2,17 @@ pragma solidity ^0.4.17;
 
 contract Adoption {
     address[16] public adopters;
+    address[17] public adopters;
 
     function adopt (uint petID) public returns(uint) {
         require(petID >= 0 && petID <= 16, "Invalid petID");
+        require(adopters[petID] == 0x0000000000000000000000000000000000000000, "Pet already adopted");
         adopters[petID] = msg.sender;
         return petID;
     }
 
     function getAdopters() public view returns(address[16]) {
+    function getAdopters() public view returns(address[17]) {
         return adopters;
     }
 }
